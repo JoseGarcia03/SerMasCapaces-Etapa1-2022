f9165cf5dc0b69108358c18df13bec1d2cafe38a
JoseGarcia03 <josegarciajaime02@gmail.com>
2022-04-04 08:34:24 -0500

    sesio 12

---
+++ Sesión 13/script.js
@@ -0,0 +1,79 @@
+//Operadores =   ==
+
+// = es de asignacion
+// let nombre = "Jose"
+
+// // == de comparacion
+// // Pregunto si nombre realmente es igual a "Jose"
+
+// // Sencillo
+// if(nombre== "Jose"){ //true
+
+// }
+
+// // Doble
+// if(){
+
+// }else{
+
+// }
+
+// // Multiple
+// if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+// let bateria = "40%"; // tipo de dato (string),  valor(40)
+// console.log(typeof(bateria))
+// let numero = "10"; // tipo de dato (string), valor (10)
+
+// if(numero === 10){
+//     console.log("es igual")
+// }else{
+//     console.log("No son iguales")
+// }
+
+// switch (numero){
+//     case 1:
+//         console.log("El numero ingresado es 1")
+//         break;
+//     case 2:
+//         console.log("El numero ingresado es 2")
+//         break;
+//     case 3:
+//         console.log("El numero ingresado es 3")
+//         break;
+//     case 7:
+//         console.log("El numero ingresado es 7")
+//         break;
+//     case 8:
+//         console.log("El numero ingresado es 8")
+//         break;
+//     default:
+//         console.log("No en encontro su numero")
+//         break;
+// }
+
+// Condicional Ternario
+let numero = 5;
+
+// ternario sencillo
+numero == 1 ? console.log("Es un 1") : console.log("no es igual a 1")
+
+// tenario sin else
+numero == 1 ? console.log("Es un 1"):
+
+// ternario multiple
+numero == 1  //Condicion
+? console.log("Es igual") //Se ejecuta si la primera es true
+: numero == 2  // Se evalua esta otra condicion
+? console.log("Es 2") // se ejecuta si la condicon es true
+: console.log("no se el numero") // SI es false se ejecuta esto
